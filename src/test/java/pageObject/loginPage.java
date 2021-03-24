69e32a4b7c0e5fcaa1d52d87df5ac3bdc76294eb
karol <karol.szczuchniak94@gmail.com>
2021-03-24 18:33:26 +0100

    update framework structure

---
+++ src/test/java/pageObject/loginPage.java
@@ -0,0 +1,81 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+//import pageObjects.ForgotPassword;
+
+public class loginPage {
+public WebDriver driver;
+	
+	By signin=By.xpath("//div[@class='user-links']/a");	
+	By email=By.xpath("//input[@id='username']");
+	By password=By.xpath("//input[@id='password']");
+	By login=By.xpath("//input[@id='kc-login']");
+	By forgotPassword = By.xpath("//*[contains(text(),'Forgot Password?')]");
+	By addAscent = By.xpath("//*[contains(text(),'Add Ascent')]");
+	By myLogbook = By.xpath("//a[contains(text(),'My Logbook')]");
+	By logOut = By.xpath("//*[contains(text(),'Logout')]");
+	
+	public loginPage(WebDriver driver) {
+	
+		this.driver=driver;
+		
+	}
+
+
+
+//public ForgotPassword forgotPassword()
+//{
+//	driver.findElement(forgotPassword).click();
+//	return new ForgotPassword(driver);
+//	
+//}
+	
+		 
+	public WebElement getEmail()
+	{
+		
+		return driver.findElement(email);
+	}
+	
+	public WebElement getPassword()
+	{
+		return driver.findElement(password);
+	}
+	
+	public WebElement getLogin()
+	{
+		return driver.findElement(login);
+	}
+	public addAscent getAddAscent()
+	{
+		driver.findElement(addAscent).click();
+		addAscent aa = new addAscent(driver);
+		return aa;
+	}
+//	public WebElement getAddAscent()
+//	{
+//		return driver.findElement(addAscent);
+//	}
+//	public WebElement getMyLogbook()
+//	{
+//		return driver.findElement(myLogbook);
+//	}
+	
+	public myLogbook getMyLogbook()
+	{
+		driver.findElement(myLogbook).click();
+		myLogbook ml = new myLogbook(driver);
+		return ml;
+	}
+	public WebElement getLogOut()
+	{
+		return driver.findElement(logOut);
+	}
+	public WebElement getLogBook()
+	{
+		return driver.findElement(myLogbook);
+	}
+}
--- target/classes/base.java
+++ target/classes/base.java
@@ -12,52 +12,52 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class base {
 
-	public  WebDriver driver;
-	public Properties prop;
+public  WebDriver driver;
+public Properties prop;
+
 public WebDriver initializeDriver() throws IOException
-{
+	{
+		
+	prop= new Properties();
+	FileInputStream fis=new FileInputStream("C:\\Users\\DELL\\eclipse-workspace\\testAuto\\src\\main\\java\\resources\\data.properties");
 	
- prop= new Properties();
-FileInputStream fis=new FileInputStream("C:\\Users\\DELL\\eclipse-workspace\\testAuto\\src\\main\\java\\resources\\data.properties");
+	prop.load(fis);
+		String browserName=prop.getProperty("browser");
+		System.out.println(browserName);
+		
+			if(browserName.equals("chrome"))
+			{
+				 System.setProperty("webdriver.chrome.driver", "./drivers\\chromedriver.exe");
+				driver= new ChromeDriver();
+			}
+			else if (browserName.equals("firefox"))
+			{
+				System.setProperty("webdriver.chrome.driver", "./drivers\\geckodriver.exe"); 
+				driver= new FirefoxDriver();
+			}
+			else if (browserName.equals("IE"))
+			{
+				System.setProperty("webdriver.chrome.driver", "./drivers\\IEDDriverServer.exe"); 
+				driver= new InternetExplorerDriver();
+			}
 
-prop.load(fis);
-String browserName=prop.getProperty("browser");
-System.out.println(browserName);
-
-if(browserName.equals("chrome"))
-{
-	 System.setProperty("webdriver.chrome.driver", "./drivers\\chromedriver.exe");
-	driver= new ChromeDriver();
-		//execute in chrome driver
-	
-}
-else if (browserName.equals("firefox"))
-{
-	 driver= new FirefoxDriver();
-	//firefox code
-}
-else if (browserName.equals("IE"))
-{
-//	IE code
-}
-
-driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-return driver;
-
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	return driver;
 
 }
 
 
 public String getScreenShotPath(String testCaseName,WebDriver driver) throws IOException
-{
-	TakesScreenshot ts=(TakesScreenshot) driver;
-	File source =ts.getScreenshotAs(OutputType.FILE);
-	String destinationFile =  System.getProperty("user.dir")+"\\reports\\"+testCaseName+".png";
-	FileUtils.copyFile(source,new File(destinationFile));
-	return destinationFile;
-}
+	{
+		TakesScreenshot ts=(TakesScreenshot) driver;
+		File source =ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile =  System.getProperty("user.dir")+"\\reports\\"+testCaseName+".png";
+		FileUtils.copyFile(source,new File(destinationFile));
+		return destinationFile;
+	}
 
 }
